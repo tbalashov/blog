258018a90a4baffc279e1d6bce72157c2574a089
Timofey Balashov <tbalashov@gmail.com>
2015-02-28 21:19:26 +0600

    Change Role (extends BaseEntity)

--- src/main/java/com/epam/tb/blog/model/Role.java
+++ src/main/java/com/epam/tb/blog/model/Role.java
@@ -1,6 +1,6 @@
 package com.epam.tb.blog.model;
 
-public class Role {
+public class Role extends BaseEntity {
     public static final Role ADMIN = new Role("admin");
     public static final Role AUTHOR = new Role("author");
     public static final Role GUEST = new Role("guest");
